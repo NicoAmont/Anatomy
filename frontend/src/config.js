c6523c136756edb40d4c8dbf3df70d3041689188
NicoAmont <nicolas.amezquita.m@gmail.com>
2021-08-30 02:10:52 -0500

    fix url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,6 @@
-export const apiUrl = 'http://localhost:4500';
+export const apiUrl = document.location.href.startsWith('http://localhost')
+  ? 'http://localhost:5000'
+  : '';
 export const formatter = new Intl.NumberFormat('en',{
     style: 'currency',
     currency: 'usd',
